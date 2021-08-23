19ff16f040c566e366a55066912493a8173f05bf
lobachaos <zwolfsasa@gmail.com>
2021-08-23 12:24:01 -0400

    Fixes

--- bin/kata/Kata06.java
+++
@@ -1,9 +0,0 @@
-package kata;
-
-public class Kata06 {
-
-	public static boolean isSquare(int n) {
-		return Math.sqrt(n) == (int) Math.sqrt(n);
-	}
-
-}
--- bin/kata/Kata08.java
+++
@@ -1,8 +0,0 @@
-package kata;
-
-public class Kata08 {
-	//https://www.codewars.com/kata/55031bba8cba40ada90011c4/train/java
-	//Hidden "Cubic" numbers
-
-
-}
