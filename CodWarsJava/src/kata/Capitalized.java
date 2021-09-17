5e0a9c940a2e6e27d375e86aee38b412c1852399
Samara Oliveira <zwolfsasa@gmail.com>
2021-09-16 22:35:13 -0400

    Create Capitalized.java

---
+++ CodWarsJava/src/kata/Capitalized.java
@@ -0,0 +1,28 @@
+package kata;
+
+
+public class Capitalized {
+    public static String camelCase(String str) {
+        if(str.isEmpty() == true){
+            return "";
+        }
+        String[] split = str.replaceAll("\\s+"," ").trim().split(" ");
+        String result=""; 
+        for(String a : split){
+          result +=  a.toString().substring(0,1).toUpperCase() + a.substring(1);
+             
+        }
+
+
+        return result.replaceAll(" ", "");
+    }
+    /*Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. 
+     *All words must have their first letter capitalized without spaces.
+     *
+     *For instance:
+     *
+     *camelCase("hello case"); // => "HelloCase"
+     * camelCase("camel case word"); // => "CamelCaseWord"
+    */
+    
+}
